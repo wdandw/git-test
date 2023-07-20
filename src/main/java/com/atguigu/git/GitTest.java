ce6fcf5868a175ec09685dc98d569a01100f0830
ws <2026236354@qq.com>
2023-07-20 17:15:20 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello sg");
         System.out.println("hello wd");
         System.out.println("hello sg4");
+        System.out.println("hello sg5");
     }
 }
