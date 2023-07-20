098b1146cbbb41edc972a5d4aa302e75920e3869
ws <2026236354@qq.com>
2023-07-20 17:08:58 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello sg");
         System.out.println("hello wd");
+        System.out.println("hello sg4");
     }
 }
