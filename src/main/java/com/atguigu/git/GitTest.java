c7a9c5026eb39109d3ced20bcf8de23ece18df35
ws <2026236354@qq.com>
2023-07-20 16:50:23 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello sg");
+        System.out.println("hello wd");
     }
 }
